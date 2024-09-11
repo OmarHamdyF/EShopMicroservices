ddec558f4ec8e19e1620021a4530e23a077248af
OmarHamdyF <o.hamdy93@gmail.com>
2024-09-11 11:51:05 +0300

    adding postgress with dockerr

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndPoint.cs
@@ -0,0 +1,23 @@
+﻿namespace CatalogAPI.Products.CreateProduct;
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+public record CreateProductResponse(Guid Id);
+public class CreateProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<CreateProductResponse>();
+            return Results.Created($"/products/{response.Id}", response);
+        })
+            .WithName("Created Products")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .WithSummary("Created Product")
+            .WithDescription("Created Product");
+    }
+}
+
