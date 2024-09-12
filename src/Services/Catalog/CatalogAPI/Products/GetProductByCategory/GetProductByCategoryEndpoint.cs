cf9f68f8722166d238f8ba23fa6a744b0a565aad
OmarHamdyF <o.hamdy93@gmail.com>
2024-09-12 16:36:14 +0300

    finished till up

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+using CatalogAPI.Products.GetProductById;
+using CatalogAPI.Products.GetProducts;
+
+namespace CatalogAPI.Products.GetProductByCategory;
+
+public record GetProductByByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByByCategoryQuery(category));
+            var response = result.Adapt<GetProductByByCategoryResponse>();
+            return Results.Ok(response);
+        }).WithName("GetProductByCategory")
+            .Produces<GetProductByByCategoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Category")
+            .WithDescription("Get Product By Category")
+            ;
+    }
+}
---
+++ src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CatalogAPI.Products.GetProductByCategory;
+
+public record GetProductByByCategoryQuery(string Category) : IQuery<GetProductByByCategoryResult>;
+public record GetProductByByCategoryResult(IEnumerable<Product> Products);
+internal class GetProductByCategoryQueryHandler(IDocumentSession session) : IQueryHandler<GetProductByByCategoryQuery, GetProductByByCategoryResult>
+{
+    public async Task<GetProductByByCategoryResult> Handle(GetProductByByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        var products = await session.Query<Product>()
+            .Where(p => p.Category.Contains(query.Category))
+            .ToListAsync();
+        return new GetProductByByCategoryResult(products);
+    }
+}
