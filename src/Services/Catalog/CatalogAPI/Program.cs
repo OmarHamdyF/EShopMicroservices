9417377fd25b5f9b5ee7f932b2a2940efa8c7d7b
OmarHamdyF <o.hamdy93@gmail.com>
2024-10-10 16:44:45 +0300

    health check ui package

--- src/Services/Catalog/CatalogAPI/Program.cs
+++ src/Services/Catalog/CatalogAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //add services to the container
@@ -22,7 +25,8 @@ if (builder.Environment.IsDevelopment())
 }
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
-builder.Services.AddHealthChecks();
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
 
 var app = builder.Build();
 
@@ -30,6 +34,9 @@ var app = builder.Build();
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
-app.UseHealthChecks("/health");
-
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 app.Run();
