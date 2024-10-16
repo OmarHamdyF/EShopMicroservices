8bd5e93786509604e07327e923a806217be4e6f7
OmarHamdyF <o.hamdy93@gmail.com>
2024-10-16 14:20:32 +0300

    Basket microservice

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,29 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketResponse(ShoppingCart cart);
+
+public class GetBasketEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            //result.cart returns wih new shopping cart item 
+            var result = await sender.Send(new GetBasketQuery(userName));
+            //i had to put this explicit mapping 
+            TypeAdapterConfig<GetBasketResult, GetBasketResponse>
+                            .NewConfig()
+                            .Map(dest => dest.cart, src => src.cart);
+
+            //although response.cart return null after this line 
+            var respose = result.Adapt<GetBasketResponse>();
+            return Results.Ok(respose);
+        })
+        .WithName("GetProductById")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Product By Id")
+        .WithDescription("Get Product By Id");
+    }
+}
+
---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string userName);
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.cart).NotNull().WithMessage("Cart cannot be null");
+        RuleFor(x => x.cart.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart car = command.cart;
+
+        return new StoreBasketResult("swn");
+    }
+}
