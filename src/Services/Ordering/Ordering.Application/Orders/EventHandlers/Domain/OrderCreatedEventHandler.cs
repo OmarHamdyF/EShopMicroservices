f4de6e78838763181bcb284036430e779dc62410
OmarHamdyF <o.hamdy93@gmail.com>
2024-11-04 16:08:18 +0300

    section 16 done

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Ordering.Application.Orders.EventHandlers.Domain;
+public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent domain, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", domain.GetType().Name);
+
+        return Task.CompletedTask;
+    }
+}
